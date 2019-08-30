7187abe6cc9101af8a93062b104b7d7166538160
Monika <moni.kesarwani@yahoo.com>
2019-08-30 12:32:23 -0400

    update code

--- lovePrediction/ViewController.swift
+++ lovePrediction/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var yourNametxt: UITextField!
+    
+    
+    @IBOutlet weak var yourPatnerNameTxt: UITextField!
+    
+    
+    
+    @IBOutlet weak var displayLbl: UILabel!
+    
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        update()
     }
+    
+    
+    
+    
 
 
+    @IBAction func goButton(_ sender: UIButton) {
+        
+        update()
+        
+    }
+    
+    func update(){
+        
+        var yourName: String = ""
+        var yourPatnerName: String = ""
+        var display: String = ""
+        yourNametxt.text = yourName
+        yourPatnerNameTxt.text = yourPatnerName
+       
+        
+        var randomNumberIndex: Int = 0
+        var randomNumberGenerator = Int.random(in: 0 ..< 100)
+         displayLbl.text = String(randomNumberGenerator)
+        
+        
+        
+    }
+    
 }
 
