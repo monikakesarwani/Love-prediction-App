ef5a01b2844944c0748be96d1df441338232ff4a
Monika <moni.kesarwani@yahoo.com>
2019-09-11 23:52:09 -0400

    changed into if else condition

--- lovePrediction/ViewController.swift
+++ lovePrediction/ViewController.swift
@@ -11,25 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    let yourName: String = ""
-    let yourPatnerName: String = ""
-    var display: String = ""
+    let yourName : String = ""
+    let yourPatnerName : String = ""
+    var display : String = ""
     var randomNumberGenerator : Int = 0
-    //var pressed: Int = 1
-    
-    
+   
     
     @IBOutlet weak var yourNametxt: UITextField!
-    
-    
     @IBOutlet weak var yourPatnerNameTxt: UITextField!
-    
-    
     @IBOutlet weak var displayLbl: UILabel!
-    
-   
     @IBOutlet weak var goButton: UIButton!
-    
     @IBOutlet weak var restartLabel: UIButton!
     
     override func viewDidLoad() {
@@ -38,45 +29,42 @@ class ViewController: UIViewController {
         
         setup()
         
-        
-        
     }
-    
-    
-    
-    
-
-
+    // when go button is pressed. if user fill all the information than excute the code otherwise show UIAlert and its action
     @IBAction func goButton(_ sender: UIButton) {
         
-        
+        if yourNametxt.text != "" && yourPatnerNameTxt.text != ""{
             yourNametxt.text = yourName
             yourPatnerNameTxt.text = yourPatnerName
             randomNumberGenerator = Int.random(in: 0 ..< 100)
             displayLbl.text = "Your love score is \(randomNumberGenerator) %"
-           // goButton.isHidden = false
             displayLbl.isHidden = false
             restartLabel.isHidden = false
-        
-
             
-        
-    
-       
-        
-    }
+        } else if  yourNametxt.text == "" && yourPatnerNameTxt.text == ""{
+            //print("Error")
+            let alert = UIAlertController(title: "Error", message: "Fill all the informations above box.", preferredStyle: .alert)
+
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { UIAlertAction in
+                self.setup()
+            })
+            alert.addAction(restartAction)
+
+            present(alert, animated: true, completion: nil)
+        }
     
 
-    
-    
-    
-    
-    
+    }
+    //when restart button is pressed. it sets up all IBOUTLET
     @IBAction func restartButton(_ sender: UIButton) {
-        setup()
+        displayLbl.isHidden = true
+        yourPatnerNameTxt.text = ""
+        yourNametxt.text = ""
+        restartLabel.isHidden = true
         
     }
     
+    //when this method is treggeried. set IBOUTLET
     func setup(){
     restartLabel.isHidden = true
     displayLbl.isHidden = true
